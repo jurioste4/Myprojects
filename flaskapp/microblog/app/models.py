bf9584d9d4181a90af38ad6dad64ce37a63b60aa
jurioste4 <jurioste4@gmail.com>
2019-08-27 14:03:52 -0500

    work on data base

--- flaskapp/microblog/app/models.py
+++ flaskapp/microblog/app/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from app import db
 
 class User(db.Model):
@@ -5,23 +6,16 @@ class User(db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     def __repr__(self):
-        return '<User {}>'.format(self.username)    
+        return '<User {}>'.format(self.username)
 
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-
-
-
-
-
-# class User (db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(64), index=True, Unique=True)
-#     email = db.Column(db.String(120), index=True, Unique=True)
-#     password_hash = db.Column(db.String(128))
-
-#     def _repr_(self):
-#         return '<User {}>'.format(self.username)
-
-    
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
