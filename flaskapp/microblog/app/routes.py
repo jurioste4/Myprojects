a2ae0dfd527b16fee9ee9b68ddc872a27a06888a
jurioste4 <jurioste4@gmail.com>
2019-08-31 00:13:09 -0500

    this is my fixing the ap

--- flaskapp/microblog/app/routes.py
+++ flaskapp/microblog/app/routes.py
@@ -1,26 +1,30 @@
-from flask import render_template
-from app import app 
+from flask import render_template, flash, redirect, url_for
+from app import app
 from app.forms import LoginForm
 
+
 @app.route('/')
-@app.route('/login')
-def login():
-    form = LoginForm()
-    
-    return render_template('login.html', title='Sign In', form=form)
 @app.route('/index')
 def index():
     user = {'username': 'Sona'}
     posts = [
         {
-            'author': {'username': 'Panda'},
-            'body': 'Beautiful day in Japan!'
+            'author': {'username': 'Sona'},
+            'body': 'Beautiful day in Denver!'
         },
         {
-            'author': {'username': 'Sona'},
-            'body': 'those Tights are so cute!!'
+            'author': {'username': 'Sara'},
+            'body': 'I love my Tights!'
         }
     ]
-    return render_template('index.html', title='Home',user=user, posts=posts)
+    return render_template('index.html', title='Home', user=user, posts=posts)
 
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html',  title='Sign In', form=form)
