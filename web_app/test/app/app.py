0cbd2a4ce79f8a06add5aff9e8212d1c94ae35e0
jurioste4 <jurioste4@gmail.com>
2020-03-11 16:45:20 -0500

    changed up my code app and index

--- web_app/test/app/app.py
+++ web_app/test/app/app.py
@@ -23,33 +23,30 @@ Base = automap_base()
 Base.prepare(db.engine, reflect=True)
 
 country = Base.classes.country
+results = db.engine.execute(
+    'SELECT Year,country,UR,Growth,inflation,ER,Population FROM country')
+df = pd.DataFrame(results, columns=[
+    'Year', 'country', 'UR', 'Growth', 'inflation', 'ER', 'Population'])
 
+usa = df[(df['country'] == 'usa')]
+china = df[(df.country == 'china')]
+uk= df[(df.country == 'uk')]
+sing= df[(df.country == 'singapore')]
+france= df[(df.country == 'france')]
 
 @app.route('/')
 def index():
-    results = db.engine.execute(
-        'SELECT Year,country,UR,Growth,inflation,ER,Population FROM country')
-    df = pd.DataFrame(results, columns=[
-                      'Year', 'country', 'UR', 'Growth', 'inflation', 'ER', 'Population'])
-
-    usa = df[(df['country'] == 'usa')]
-    china = df[(df.country == 'china')]
+   
     graph = dict(
-        data=[go.Bar(
-            x=usa["Year"],
-            y=usa["UR"]
-        ),
-            go.Bar(name='China',
-                   x=china.Year,
-                   y=china['UR'])],
-        layout=dict(barmode='group',
-                    title='test',
-                    yaxis=dict(
-                        title="UR"
-                    ),
-                    xaxis=dict(
-                        title="Year"
-                    )
+        data=[go.Bar(name='france',x=france["Year"],y=france["UR"]),
+            go.Bar(name='singapore',x=sing["Year"],y=sing["UR"]),
+            go.Bar(name='UK',x=uk["Year"],y=uk["UR"]),
+            go.Bar(name='USA',x=usa["Year"], y=usa["UR"]),
+            go.Bar(name='China', x=china.Year, y=china['UR']
+              )],
+        layout=dict(barmode='group', title='US unemployment rate vs Four Top Countries',
+                    yaxis=dict(title="Unemployment Rate" ),
+                    xaxis=dict(title="Year" )
                     )
     )
 
@@ -57,6 +54,26 @@ def index():
 
     return render_template('layouts/index.html', graphJSON=graphJSON)
 
+@app.route("/data")
+def data():
+    graph = dict(
+        data=[go.Scatter(name='france',x=france["Year"],y=france["UR"],mode='lines'),
+            go.Scatter(name='singapore',x=sing["Year"],y=sing["UR"],mode='lines'),
+            go.Scatter(name='UK',x=uk["Year"],y=uk["UR"],mode='lines'),
+            go.Scatter(name='USA',x=usa["Year"], y=usa["UR"],mode='lines'),
+            go.Scatter(name='China', x=china.Year, y=china['UR'],mode='lines')],
+        layout=dict(title='US unemployment rate vs Four Top Countries',
+                    yaxis=dict(
+                        title="Unemployment Rate"
+                    ),
+                    xaxis=dict(
+                        title="Year"
+                    )
+                    )
+    )
 
+    graphJSON_two= json.dumps(graph, cls=plotly.utils.PlotlyJSONEncoder)
+
+    return  (graphJSON_two)
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True, port=3134)
+    app.run()
