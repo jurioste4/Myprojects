1cee025651ff0c94a2dc88b2ec4c409ed0069a07
jurioste4 <jurioste4@gmail.com>
2021-03-21 22:42:00 -0700

    finishing dash work

--- python_practice/plotly_dash/index.py
+++ python_practice/plotly_dash/index.py
@@ -1,24 +1,28 @@
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
-from dash_html_components.P import P
-
 
 from app import app, server
-
+# import your navigation, styles and layouts from layouts.py here
 from layouts import nav_bar, layout1, layout2, CONTENT_STYLE
 import callbacks
 
-
+# Define basic structure of app:
+# A horizontal navigation bar on the left side with page content on the right.
 app.layout = html.Div([
+    # this locates this structure to the url
     dcc.Location(id='url', refresh=False),
     nav_bar(),
+    # we'll use a callback to change the layout of this section
     html.Div(id='page-content', style=CONTENT_STYLE)
 ])
 
+# This callback changes the layout of the page based on the URL
+# For each layout read the current URL page "http://127.0.0.1:5000/pagename" and return the layout
 
-@app.callback(Output('page-content', 'children')
-              [Input('url', 'pathname')])
+
+@app.callback(Output('page-content', 'children'),  # this changes the content
+              [Input('url', 'pathname')])  # this listens for the url in use
 def display_page(pathname):
     if pathname == '/':
         return layout1
@@ -27,8 +31,9 @@ def display_page(pathname):
     elif pathname == '/page2':
         return layout2
     else:
-        return '404'
+        return '404'  # If page not found return 404
 
 
+# Runs the server at http://127.0.0.1:5000/
 if __name__ == '__main__':
     app.run_server(port=5000, host='127.0.01', debug=True)
--- python_practice/plotly_dash/layouts.py
+++ python_practice/plotly_dash/layouts.py
@@ -1,9 +1,21 @@
+import dash_core_components as dcc
+import dash_html_components as html
+import dash_bootstrap_components as dbc
 from app import app
 import plotly.express as px
+import plotly.graph_objects as go
 
 
+#####################################
+# Add your data
+#####################################
+
+# example iris dataset
 df = px.data.iris()
 
+#####################################
+# Styles & Colors
+#####################################
 
 NAVBAR_STYLE = {
     "position": "fixed",
@@ -13,16 +25,18 @@ NAVBAR_STYLE = {
     "width": "16rem",
     "padding": "2rem 1rem",
     "background-color": "#f8f9fa",
-
 }
 
 CONTENT_STYLE = {
     "top": 0,
-    "margin_top": '2rem',
-    "margin-left": '18rem',
-    "margin-right": '2rem',
-
+    "margin-top": '2rem',
+    "margin-left": "18rem",
+    "margin-right": "2rem",
 }
+
+#####################################
+# Create Auxiliary Components Here
+#####################################
 
 
 def nav_bar():
@@ -31,27 +45,127 @@ def nav_bar():
     """
     navbar = html.Div(
         [
-            html.H4("System Preformance dashboard",
-                    ClassName="display-10", style={'textAlign': 'center'}),
+            html.H4("System Performance Dashboard",
+                    className="display-10", style={'textAlign': 'center'}),
             html.Hr(),
             dbc.Nav(
                 [
                     dbc.NavLink("Page 1", href="/page1",
                                 active="exact", external_link=True),
-                    dbc.NavLink("Page 2", href="/page1",
+                    dbc.NavLink("Page 2", href="/page2",
                                 active="exact", external_link=True)
-
                 ],
                 pills=True,
                 vertical=True
             ),
         ],
         style=NAVBAR_STYLE,
-
     )
-    return nav_bar
+
+    return navbar
 
 
-# Graph1
+# graph 1
 example_graph1 = px.scatter(
     df, x="sepal_length", y="sepal_width", color="species")
+
+# graph 2
+example_graph2 = px.histogram(df, x="sepal_length", color="species", nbins=20)
+example_graph2.update_layout(barmode='overlay')
+example_graph2.update_traces(opacity=0.55)
+
+
+#####################################
+# Create Page Layouts Here
+#####################################
+
+# Layout 1
+layout1 = html.Div([
+    html.H2("Page 1"),
+    html.Hr(),
+    # create bootstrap grid
+    dbc.Container([
+        dbc.Row(
+            [
+                dbc.Col(
+                    [
+                        html.Div(
+                            [
+                                html.H4('Example Graph Page'),
+                                # create tabs
+                                dbc.Tabs(
+                                    [
+                                        dbc.Tab(label='graph1',
+                                                tab_id='graph1'),
+                                        dbc.Tab(label='graph2',
+                                                tab_id='graph2')
+                                    ],
+                                    id="tabs",
+                                    active_tab='graph1',
+                                ),
+                                html.Div(id="tab-content", className="p-4")
+                            ]
+                        ),
+                    ],
+                    width=6  # half page
+                ),
+
+                dbc.Col(
+                    [
+                        html.H4('Additional Components here'),
+                        html.P('Click on graph to display text', id='graph-text')
+                    ],
+                    width=6  # half page
+                )
+
+            ],
+        ),
+    ]),
+])
+
+
+# Layout 2
+layout2 = html.Div(
+    [
+        html.H2('Page 2'),
+        html.Hr(),
+        dbc.Container(
+            [
+                dbc.Row(
+                    [
+                        dbc.Col(
+                            [
+                                html.H4('Country'),
+                                html.Hr(),
+                                dcc.Dropdown(
+                                    id='page2-dropdown',
+                                    options=[
+                                        {'label': '{}'.format(i), 'value': i} for i in [
+                                            'United States', 'Canada', 'Mexico'
+                                        ]
+                                    ]
+                                ),
+                                html.Div(id='selected-dropdown')
+                            ],
+                            width=6
+                        ),
+                        dbc.Col(
+                            [
+                                html.H4('Fruit'),
+                                html.Hr(),
+                                dcc.RadioItems(
+                                    id='page2-buttons',
+                                    options=[
+                                        {'label': '{}'.format(i), 'value': i} for i in [
+                                            'Yes ', 'No ', 'Maybe '
+                                        ]
+                                    ]
+                                ),
+                                html.Div(id='selected-button')
+                            ],
+                        )
+                    ]
+                ),
+            ]
+        )
+    ])
